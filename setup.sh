e436c1aff7ca807cb00bfb3ba6cf826d53a8ee14
Christopher Carlisle <carlic578@gmail.com>
2013-05-08 23:41:03 -0500

    Working on vi setup

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git submodule add git@github.com:vim-scripts/CSApprox.git vi/vim.symlink/bundle/CSApprox
+git submodule add git@github.com:scrooloose/nerdcommenter.git vi/vim.symlink/bundle/nerdcommenter
+git submodule add git@github.com:scrooloose/nerdtree.git vi/vim.symlink/bundle/nerdtree
+git submodule add git@github.com:Lokaltog/powerline.git vi/vim.symlink/bundle/powerline
+git submodule add git@github.com:vim-scripts/ScrollColors.git vi/vim.symlink/bundle/ScrollColors
+git submodule add git@github.com:ervandew/supertab.git vi/vim.symlink/bundle/supertab
+git submodule add git@github.com:majutsushi/tagbar.git vi/vim.symlink/bundle/tagbar
+git submodule add https://github.com/tomtom/tlib_vim.git vi/vim.symlink/bundle/tlib_vim
+git submodule add https://github.com/MarcWeber/vim-addon-mw-utils.git vi/vim.symlink/bundle/vim-addon-mw-utils
+git submodule add git://github.com/nathanaelkane/vim-indent-guides.git vi/vim.symlink/bundle/vim-indent-guides
+git submodule add git://github.com/garbas/vim-snipmate.git vi/vim.symlink/bundle/vim-snipmate
+git submodule add https://github.com/honza/vim-snippets.git vi/vim.symlink/bundle/vim-snippets
+git submodule add git@github.com:nathanaelkane/vim-indent-guides.git vi/vim.symlink/bundle/vim-indent-guides
